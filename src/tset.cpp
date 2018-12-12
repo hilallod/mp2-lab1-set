4571b836286a6b27a8ffbcc7345004488d9c60af
Широков Сергей Александрович <shirokov.s@ccam.cmc.unn.net>
2018-12-12 15:50:24 +0300

    zz

--- src/tset.cpp
+++ src/tset.cpp
@@ -2,7 +2,7 @@
 //
 // tset.cpp - Copyright (c) Гергель В.П. 04.10.2001
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
+// Работу над шаблонами выполнил студент группы 381706-4 Серобян Нарек.
 // Множество - реализация через битовые поля
 
 #include "tset.h"
@@ -17,7 +17,7 @@ TSet::TSet(const TSet &s) : BitField(s.BitField), MaxPower(s.MaxPower)
 }
 
 // конструктор преобразования типа
-TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower(bf.GetLength())
+TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower( bf.GetLength() )
 {
 
 
@@ -51,30 +51,30 @@ void TSet::DelElem(const int Elem) // исключение элемента мн
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
-	MaxPower = s.MaxPower;
-	BitField = s.BitField;
+	MaxPower=s.MaxPower;
+	BitField=s.BitField;
 	return *this;
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
 {
-	if (MaxPower == s.MaxPower)
-	if (BitField == s.BitField)
-		return 1;
-	return 0;
+	if(MaxPower==s.MaxPower)
+		if(BitField==s.BitField)
+			return 1;
+    return 0;
 }
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
-	if (MaxPower == s.MaxPower)
-	if (BitField == s.BitField)
-		return 0;
-	return 1;
+	if(MaxPower==s.MaxPower)
+		if(BitField==s.BitField)
+			return 0;
+    return 1;
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
-	return (BitField | s.BitField);
+	return (BitField|s.BitField);
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
